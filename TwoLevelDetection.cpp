bf7f8f65bf276fafdad7d7d2c9d1b40b30e83276
Juha Reunanen <juha.reunanen@tomaattinen.com>
2016-03-03 12:08:59 +0200

    Add minContourLength parameter.

--- TwoLevelDetection.cpp
+++ TwoLevelDetection.cpp
@@ -1,13 +1,13 @@
 #include "TwoLevelDetection.h"
 #include <opencv2/imgproc/imgproc.hpp>
 
-void TwoLevelDetection(const cv::Mat& input, cv::Mat& output, double detectionLevel, double measurementLevel, TwoLevelDetectionTemp& temp)
+void TwoLevelDetection(const cv::Mat& input, cv::Mat& output, const TwoLevelDetectionParameters& parameters, TwoLevelDetectionTemp& temp)
 {
-    if (measurementLevel > detectionLevel) {
+    if (parameters.measurementLevel > parameters.detectionLevel) {
         throw std::runtime_error("TwoLevelDetection: it is required that measurementLevel <= detectionLevel");
     }
 
-    cv::threshold(input, output, measurementLevel, 1, cv::THRESH_BINARY);
+    cv::threshold(input, output, parameters.measurementLevel, 1, cv::THRESH_BINARY);
 
     temp.contours.clear();
     cv::findContours(output, temp.contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
@@ -17,14 +17,22 @@ void TwoLevelDetection(const cv::Mat& input, cv::Mat& output, double detectionLe
     output.setTo(0);
 
     for (size_t i = 0, end = temp.contours.size(); i < end; ++i) {
-        temp.mask.setTo(0);
-        cv::drawContours(temp.mask, temp.contours, static_cast<int>(i), 255, -1);
+        if (temp.contours[i].size() >= parameters.minContourLength) {
+            temp.mask.setTo(0);
+            cv::drawContours(temp.mask, temp.contours, static_cast<int>(i), 255, -1);
 
-        double minVal, maxVal;
-        cv::minMaxIdx(input, &minVal, &maxVal, NULL, NULL, temp.mask);
+            double minVal, maxVal;
+            cv::minMaxIdx(input, &minVal, &maxVal, NULL, NULL, temp.mask);
 
-        if (maxVal >= detectionLevel) {
-            output.setTo(std::numeric_limits<unsigned char>::max(), temp.mask);
+            if (maxVal >= parameters.detectionLevel) {
+                output.setTo(std::numeric_limits<unsigned char>::max(), temp.mask);
+            }
         }
     }
+}
+
+// A convenience wrapper for backward compatibility. TODO: remove this.
+void TwoLevelDetection(const cv::Mat& input, cv::Mat& output, double detectionLevel, double measurementLevel, TwoLevelDetectionTemp& temp)
+{
+    TwoLevelDetection(input, output, TwoLevelDetectionParameters(detectionLevel, measurementLevel), temp);
 }
